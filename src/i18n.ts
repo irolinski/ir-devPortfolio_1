3aebf2cc1d4aaf75c42bddd76c1d78e948cdd7ab
irolinski <ignacy.rolinski@gmail.com>
2025-06-20 17:32:10 +0200

    feat: add and init i18n

---
+++ src/i18n.ts
@@ -0,0 +1,17 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enResources from "./locales/en.json";
+
+const resources = {
+  en: { translation: enResources },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  interpolation: {
+    escapeValue: false, // react already safes from xss
+  },
+});
+
+export default i18n;
