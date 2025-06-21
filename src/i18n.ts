2d65e88e187c8571d865b924f586a377f1308419
irolinski <ignacy.rolinski@gmail.com>
2025-06-21 17:09:24 +0200

    feat: add pl locales; get selected lang from url

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,28 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enResources from "./locales/en.json";
+import plResources from "./locales/pl.json";
+
+const getLanguageFromURL = () => {
+  const params = new URLSearchParams(window.location.search);
+  const lang = params.get("lang");
+  console.log(lang);
+
+  return lang && ["en", "pl"].includes(lang) ? lang : null;
+};
+
+const selectedLanguage = getLanguageFromURL() || "en";
 
 const resources = {
   en: { translation: enResources },
+  pl: { translation: plResources },
 };
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: "en",
+  lng: selectedLanguage,
+  supportedLngs: ["en", "pl"],
+  fallbackLng: ["en"],
   interpolation: {
     escapeValue: false, // react already safes from xss
   },
